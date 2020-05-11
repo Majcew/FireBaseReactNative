4837f3d6bd2a4d20c2324fdb2f4282a910e61ed0
Majcew <48514316+Majcew@users.noreply.github.com>
2020-05-11 20:34:51 +0200

    added a gif for the loading screen + forced 5 seconds to wait for login screen

--- src/screens/LoadingScreen.js
+++ src/screens/LoadingScreen.js
@@ -1,18 +1,26 @@
 import React, { useEffect } from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import firebase from "firebase";
 
 const LoadingScreen = ({ navigation }) => {
   useEffect(() => {
-    firebase.auth().onAuthStateChanged((user) => {
-      navigation.navigate(user ? "App" : "Auth");
-    });
+    setTimeout(() => {
+      firebase.auth().onAuthStateChanged((user) => {
+        navigation.navigate(user ? "App" : "Auth");
+      });
+    }, 5000);
   }, []);
 
   return (
     <View style={styles.container}>
-      <Text>Loading Screen!</Text>
-      <ActivityIndicator size="large"></ActivityIndicator>
+      <Text style>Loading the app!</Text>
+      <Image
+        style={{ width: 300, height: 200 }}
+        source={{
+          uri:
+            "https://i.pinimg.com/originals/71/0f/da/710fda642bedd21a8279e3c1899f11e8.gif",
+        }}
+      />
     </View>
   );
 };
@@ -22,7 +30,6 @@ export default LoadingScreen;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
