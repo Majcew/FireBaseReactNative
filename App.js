a7c105e6e08ecb8bd5f673614ed4a2293dc106c0
Majcew <48514316+Majcew@users.noreply.github.com>
2020-05-11 11:22:51 +0200

    initial commit

--- App.js
+++ App.js
@@ -1,19 +1,21 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import AppNavigation from "./src/navigation/AppNavigation";
+import firebase from "firebase";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+  const firebaseConfig = {
+    apiKey: "AIzaSyD1LoRQ1SleKp4hU7m2ApeRlU9mVXuLoPQ",
+    authDomain: "feuerbase-6356c.firebaseapp.com",
+    databaseURL: "https://feuerbase-6356c.firebaseio.com",
+    projectId: "feuerbase-6356c",
+    storageBucket: "feuerbase-6356c.appspot.com",
+    messagingSenderId: "161084976380",
+    appId: "1:161084976380:web:8e3cebdda1888cde30fb79",
+  };
+  // Initialize Firebase
+  !firebase.apps.length
+    ? firebase.initializeApp(firebaseConfig)
+    : firebase.app();
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  return <AppNavigation />;
+}
